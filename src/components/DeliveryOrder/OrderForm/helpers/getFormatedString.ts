c055739e31332a5bb3b0962fe584c018a723ed26
BytesHouse <a.lihtgolt@gmail.com>
2024-07-13 20:15:00 +0300

    test

--- src/components/DeliveryOrder/OrderForm/helpers/getFormatedString.ts
+++
@@ -1,4 +0,0 @@
-export const getFormatedString = (arg: string) => {
-const tmp =  arg.replace(/[\[\]{}""]/g, '')
-return tmp.replaceAll(',', '\n')
-}
--- src/components/Desktop/components/ProductCardDesktop/ProductCardDesktop.tsx
+++
@@ -1,6 +0,0 @@
-const ProductCardDesktop = ({ product }: any) => {
-    return <li className="border m-2 p-[5px]">{product.name}</li>;
-};
-
-export default ProductCardDesktop;
-
